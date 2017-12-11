3e67654b1677cb24f383bf80333f66e4ea8e7a28
sevta <sevtasaputra@gmail.com>
2017-12-11 23:15:40 +0700

    add pro player section

--- component/Hero.js
+++ component/Hero.js
@@ -46,7 +46,7 @@ const Screen1 = props =>
 							<Text note> Lorem ipsum dolor sit amet, consectetur </Text>
 						</Body>
 						<Right>
-							<Button style={{marginTop: 12 , backgroundColor: 'dodgerblue'}} rounded success small onPress={() => props.press(item)}>
+							<Button style={{marginTop: 12 , backgroundColor: themes.primary}} rounded success small onPress={() => props.press(item)}>
 								<Text note style={{color: 'white'}}>View</Text>
 							</Button>	
 						</Right>
@@ -56,6 +56,63 @@ const Screen1 = props =>
 			</List>
 		</Content>
 	</Container>
+
+class Screen2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pro_players: [],
+      loading: false
+    };
+  }
+  componentWillMount() {
+    this.setState({ loading: true })
+    axios.get('https://api.opendota.com/api/proPlayers')
+      .then(res => {
+        this.setState({ pro_players: res.data } , () => {
+          this.setState({ loading: false })
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  proPlayers() {
+    const { pro_players } = this.state
+    return (
+      <Container>
+        <Content>
+          <List dataArray={pro_players} 
+            renderRow={(item) => (
+              <ListItem avatar>
+                <Left>
+                  <Thumbnail size={30} source={{uri: `${item.avatarfull} === 'null' ? https://vignette.wikia.nocookie.net/dragcave/images/6/6e/No_avatar.jpg/revision/latest?cb=20140427211725 : ${item.avatarfull}`}} />
+                </Left>
+                <Body>
+                  <Text>{item.name}</Text>
+                  <Text note>{item.personaname}</Text>
+                  <Text note>Team {item.team_name}</Text>
+                </Body>
+                <Right>
+                </Right>
+              </ListItem>
+            )}
+          />
+        </Content>
+      </Container>
+    )
+  }
+
+  render() {
+    const { loading } = this.state
+    return loading ? (
+      <View style={{flex: 1 , justifyContent: 'center' , alignItems: 'center'}}>
+        <ActivityIndicator />
+      </View>
+    ) : this.proPlayers() 
+  }
+}
 
 class Home extends Component {
 	constructor(props) {
@@ -84,7 +141,7 @@ class Home extends Component {
 		      <Header hashTabs
 		        noShadow={true}
 		        style={{backgroundColor: themes.primary}}
-		        androidStatusBarColor='dodgerblue'
+		        androidStatusBarColor={themes.primary}
 		      >
 		        <Left>
 		          <Button transparent onPress={() => this.props.navigation.navigate('DrawerOpen')}>
@@ -92,7 +149,7 @@ class Home extends Component {
 		          </Button>
 		        </Left>
 		        <Body style={{marginLeft: -130}}>
-		          <Text style={{color: 'white' , fontSize: 18}}>Home</Text>
+		          <Text style={{color: 'white' , fontSize: 18 , fontWeight: '600'}}>Home</Text>
 		        </Body>
 		      </Header>
 		      <Tabs>
@@ -105,12 +162,12 @@ class Home extends Component {
 			        		heroes={this.state.heroes} 
 			        		press={(item) => this.props.navigation.navigate('single' , {heroes: item})} />
 		        </Tab>
-		        <Tab heading='ALl'
+		        <Tab heading='Pro Players'
 		          textStyle={{color: 'white'}}
 		          tabStyle={{backgroundColor: themes.primary}}
 		          activeTabStyle={{backgroundColor: themes.primary}}
 		          activeTextStyle={{color: 'white'}} >
-			          <Text>Hay</Text>
+			          <Screen2 />
 		        </Tab>
 		      </Tabs>
 		    </Container>	
--- style/index.js
+++ style/index.js
@@ -1,3 +1,3 @@
 export const themes = {
-	primary: 'dodgerblue',
+	primary: 'deepskyblue',
 }
